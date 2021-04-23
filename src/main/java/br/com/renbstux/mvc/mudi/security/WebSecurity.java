5da74f1169c50b777bf7e0847c86296759e75ee9
Renato de Souza <renbstux@gmail.com>
2021-04-23 17:09:44 -0300

    Login e Logout

---
+++ src/main/java/br/com/renbstux/mvc/mudi/security/WebSecurity.java
@@ -0,0 +1,45 @@
+package br.com.renbstux.mvc.mudi.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurity extends WebSecurityConfigurerAdapter {
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+		.authorizeRequests()
+			.anyRequest().authenticated()
+		.and()
+			.formLogin(form -> form
+	            .loginPage("/login")
+	            .permitAll()
+	        )
+			.logout(logout -> logout.logoutUrl("/logout")
+			);
+		
+	}
+	
+	@Bean
+	@Override
+	public UserDetailsService userDetailsService() {
+		UserDetails user =
+			 User.withDefaultPasswordEncoder()
+				.username("renato")
+				.password("12345")
+				.roles("ADM")
+				.build();
+
+		return new InMemoryUserDetailsManager(user);
+	}
+
+}
