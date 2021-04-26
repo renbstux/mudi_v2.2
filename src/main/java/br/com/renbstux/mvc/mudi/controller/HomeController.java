e46a31b7bfd390ce785b86a2d8646b0990246430
Renato de Souza <renbstux@gmail.com>
2021-04-26 20:40:54 -0300

    Managing pagination

--- src/main/java/br/com/renbstux/mvc/mudi/controller/HomeController.java
+++ src/main/java/br/com/renbstux/mvc/mudi/controller/HomeController.java
@@ -4,11 +4,11 @@ import java.security.Principal;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import br.com.renbstux.mvc.mudi.model.Pedido;
@@ -24,7 +24,10 @@ public class HomeController {
 	
 	@GetMapping()
 	public String home(Model model, Principal principal) {
-		List<Pedido> pedidos = repository.findByStatus(StatusPedido.ENTREGUE);
+		
+		Sort sort = Sort.by("dataDaEntrega").descending();
+		PageRequest paginacao = PageRequest.of(0, 10, sort);
+		List<Pedido> pedidos = repository.findByStatus(StatusPedido.ENTREGUE, paginacao);
 		model.addAttribute("pedidos", pedidos);
 		return "home"; 
 	}
